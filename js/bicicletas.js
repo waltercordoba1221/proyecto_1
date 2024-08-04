3930527d29fdb68eb821c3ef2b170026c3e77b9e
waltercordoba1221 <waltercordoba2014@gmail.com>
2024-08-04 12:45:35 -0300

    primer commit

---
+++ js/bicicletas.js
@@ -0,0 +1,32 @@
+const bicicletas = [
+  {
+    id:1,
+    nombre: "Remera",
+    precio: 7200,
+  },
+  {
+    id:2,
+    nombre: "Remera",
+    precio: 4500,
+  },
+  {
+    id:3,
+    nombre: "Remera",
+    precio: 8000,
+  },
+  {
+    id:4,
+    nombre: "Pantalon",
+    precio: 6000,
+  },
+  {
+    id:5,
+    nombre: "Pantalon",
+    precio: 7000,
+  },
+  {
+    id:6,
+    nombre: "Pantalon",
+    precio: 3500,
+  }
+]
